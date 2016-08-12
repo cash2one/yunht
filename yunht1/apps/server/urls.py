0ad8cb71e03f32323266ad54856efb0b088ceb97
jiaolj <841232468@qq.com>
2016-08-12 13:43:39 +0800

    Signed-off-by: jiaolj <841232468@qq.com>

--- yunht1/apps/server/urls.py
+++ yunht1/apps/server/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('apps.server.views',
     url(r'^class$', 'cls'),
+    url(r'^class2$', 'cls2'),
 )
--- yunht1/apps/server/views.py
+++ yunht1/apps/server/views.py
@@ -3,6 +3,12 @@ from tools.func import to_tmp,to_json
 
 pubTitle='云荷廷健康管理'
 def cls(req):
-    seoTitle = '美国试管婴儿_泰国试管婴儿_' + pubTitle
+    #seoTitle = '美国试管婴儿_泰国试管婴儿_' + pubTitle
+    seoTitle = '美国代孕竟然是这样的？美国试管婴儿有什么好？点击为您揭秘！'
     active = 'server/home'
+    return to_tmp(active,req,locals())
+
+def cls2(req):
+    seoTitle = '美国代孕竟然是这样的？美国试管婴儿有什么好？点击为您揭秘！'
+    active = 'server/home2'
     return to_tmp(active,req,locals())
